49b26d9a7240145f05bdbe9c9c894e2aedb54078
masud040 <masudinfo962@gmail.com>
2025-01-28 17:49:40 +0600

    feat: add review tracker in traking order

--- app/components/track-order/order-process-category/ToReviewOrders.js
+++ app/components/track-order/order-process-category/ToReviewOrders.js
@@ -1,3 +1,32 @@
+import { NoDataFound } from "../../shared/NoDataFound";
+
 export default function ToReviewOrders({ reviewedItems }) {
-  return <div>To Review Orders Page</div>;
+  return (
+    <div>
+      <h2 className="mb-6 h5-md-h4-medium">Reviewed Orders</h2>
+
+      {reviewedItems.length > 0 ? (
+        reviewedItems.map((order) => (
+          <div key={order.id} className="space-y-8">
+            {order.products.map((product) => (
+              <OrderedProductCard
+                key={product._id}
+                productData={{
+                  ...product,
+                  createdAt: order.createdAt,
+                  orderId: order.orderId,
+                  mainId: order.id,
+                  status: order.status,
+                  ongoing_status: order.ongoing_status,
+                  userId: order.userId,
+                }}
+              />
+            ))}
+          </div>
+        ))
+      ) : (
+        <NoDataFound message="There is no orders placed yet." cart={true} />
+      )}
+    </div>
+  );
 }
