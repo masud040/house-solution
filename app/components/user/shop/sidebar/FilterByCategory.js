20e2952cb15d07763e220e104d6afc8f8569c97c
masud040 <masudinfo962@gmail.com>
2025-02-20 13:27:26 +0600

    fix issue on shop page

--- app/components/user/shop/sidebar/FilterByCategory.js
+++ app/components/user/shop/sidebar/FilterByCategory.js
@@ -10,34 +10,33 @@ export const FilterByCategory = ({ categories }) => {
   const params = new URLSearchParams(searchParams);
   const [query, setQuery] = useState([]);
 
+  useEffect(() => {
+    let category = decodeURI(searchParams?.get("category")?.toLowerCase());
+
+    const selectedCategory = category
+      .split("|")
+      .filter((item) => item !== "undefined" && item !== "");
+    setQuery(selectedCategory);
+  }, [searchParams]);
   function handleChange(e) {
-    const name = e.target.name;
+    const name = e.target.name.toLowerCase();
     const checked = e.target.checked;
     if (checked) {
-      setQuery([...query, name.toLowerCase()]);
+      setQuery((prevQuery) => [...prevQuery, name]);
     } else {
-      const filtered = query.filter((item) => item !== name.toLowerCase());
-
-      setQuery(filtered);
+      setQuery((prevQuery) => prevQuery.filter((item) => item !== name));
     }
   }
 
   useEffect(() => {
-    let category = decodeURI(searchParams?.get("category")?.toLowerCase());
-    const selectedCategory = category.split("|");
-    setQuery(selectedCategory);
-  }, []);
-  useEffect(() => {
+    const newParams = new URLSearchParams(searchParams);
     if (query.length > 0) {
-      params.set(
-        "category",
-        encodeURI(query.filter((item) => item !== "undefined").join("|"))
-      );
+      newParams.set("category", encodeURI(query.join("|")));
     } else {
-      params.delete("category");
+      newParams.delete("category");
     }
-    replace(`${pathname}?${params.toString()}`);
-  }, [query, pathname, replace]);
+    replace(`${pathname}?${newParams.toString()}`);
+  }, [query, pathname, replace, searchParams]);
 
   return (
     <div>
@@ -45,21 +44,34 @@ export const FilterByCategory = ({ categories }) => {
       <div className="space-y-2">
         {categories?.map((category) => (
           <div key={category.name} className="flex-between">
-            <div className="gap-2 flex-start">
+            <div className="flex items-center gap-2">
               <input
                 onChange={(e) => handleChange(e)}
                 type="checkbox"
                 name={category.name}
-                id={category.name}
-                className="rounded-sm cursor-pointer text-primary focus:ring-0"
+                id={category.name.toLowerCase()}
+                className="hidden peer"
                 checked={query.includes(category?.name?.toLowerCase())}
               />
               <label
-                htmlFor={category.name}
-                className="font-medium cusror-pointer"
+                htmlFor={category.name.toLowerCase()}
+                className="flex items-center justify-center w-5 h-5 border-2 rounded-md cursor-pointer border-primary peer-checked:bg-primary peer-checked:border-primary peer-focus:ring-2 peer-focus:ring-offset-2 peer-focus:ring-primary"
               >
-                {category?.name}
+                {/* Check Icon */}
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="w-4 h-4 text-white peer-checked:block"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 111.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                    clipRule="evenodd"
+                  />
+                </svg>
               </label>
+              <p>{category.name}</p>
             </div>
             <div className="text-sm font-semibold">
               ({category?.products ? category.products : 0})
